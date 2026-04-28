d1562bb7c8a691ad8127c81513e37a613247cde7
Adja Soukeye Faye <fadjasoukeye@gmail.com>
2026-04-28 16:49:12 +0000

    lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+    return (
+        <div className="statistique">
+            <h2 className="stat-nombre">10</h2>
+            <p className="stat-label">lignes</p>
+        </div>
+    );
+}
+
+export default Statistique;
